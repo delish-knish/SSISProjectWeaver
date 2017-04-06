1ba7f56b5ac176091cbef8a885ed1bcc7014dcbc
David Elish <david.elish@informationcollaboration.com>
2017-04-06 13:33:42 -0700

    Naming fix.

--- SSISExecutionManager/Stored Procedures/sqlcmd/CheckAndWaitForCompetingJobs.sql
+++ SSISExecutionManager/Stored Procedures/sqlcmd/CheckAndWaitForCompetingJobs.sql
@@ -1,34 +1,38 @@
 ﻿CREATE PROCEDURE [sqlcmd].[CheckAndWaitForCompetingJobs] @CallingJobName          VARCHAR(128),
-                                                         @CallingJobNameToWaitFor VARCHAR(128),
-                                                         @ETLBatchId               INT,
-														 @EndETLBatchExecutionInd BIT OUT
+                                                         @JobNameToWaitFor        VARCHAR(128),
+                                                         @ETLBatchExecutionId     INT,
+                                                         @EndETLBatchExecutionInd BIT OUT
 AS
-    DECLARE @EventDescription          VARCHAR(MAX),
-            @PollingDelay			   CHAR(8) = ( [dbo].[func_GetConfigurationValue] ('Default SQL Command Trigger Polling Delay') ),
-            @BatchStartedWithinMinutes INT = ( [dbo].[func_GetConfigurationValue] ('Minutes Back to Continue a Batch') ),
-            @ErrorEmailRecipients      VARCHAR(MAX) = ( [dbo].[func_GetConfigurationValue] ('Email Recipients - Default') ),
-            @SendTimeoutNotification   BIT = ( [dbo].[func_GetConfigurationValue] ('Send Timeout Notifications') ),
-            @BatchDayOfWeekName        VARCHAR(10),
-            @BatchStartDateTime        DATETIME2,
-            @ETLBatchTimeOutStatusId   INT = 8,
-            @TimeoutEmailSubject       VARCHAR(MAX) = @@SERVERNAME + ' ' + @CallingJobName + ' Timed Out';
+    DECLARE @EventDescription           VARCHAR(MAX)
+            ,@PollingDelay              CHAR(8) = ([dbo].[func_GetConfigurationValue] ('Default SQL Command Trigger Polling Delay'))
+            ,@BatchStartedWithinMinutes INT = ([dbo].[func_GetConfigurationValue] ('Minutes Back to Continue a Batch'))
+            ,@ErrorEmailRecipients      VARCHAR(MAX) = ([dbo].[func_GetConfigurationValue] ('Email Recipients - Default'))
+            ,@SendTimeoutNotification   BIT = ([dbo].[func_GetConfigurationValue] ('Send Timeout Notifications'))
+            ,@BatchDayOfWeekName        VARCHAR(10)
+            ,@BatchStartDateTime        DATETIME2
+            ,@ETLBatchTimeOutStatusId   INT = 8
+            ,@TimeoutEmailSubject       VARCHAR(MAX) = @@SERVERNAME + ' ' + @CallingJobName + ' Timed Out';
 
     --Get running ETLBatch
     SELECT
       @BatchDayOfWeekName = DayOfWeekName
-      ,@BatchStartDateTime = StartDateTime
+     ,@BatchStartDateTime = StartDateTime
     FROM
       [dbo].[func_GetRunningETLBatchExecution] (@BatchStartedWithinMinutes, @CallingJobName) eb
 
-    WHILE [dbo].[func_IsSQLAgentJobRunning](@CallingJobNameToWaitFor) = 1
+    WHILE [dbo].[func_IsSQLAgentJobRunning](@JobNameToWaitFor) = 1
       BEGIN
-          SET @EventDescription = 'Waiting for [' + @CallingJobNameToWaitFor + '] job to complete. Wait interval ' + @PollingDelay;
+          SET @EventDescription = 'Waiting for [' + @JobNameToWaitFor + '] job to complete. Wait interval ' + @PollingDelay;
 
-          EXEC [log].InsertETLBatchEvent 10,@ETLBatchId,NULL,@EventDescription;
+          EXEC [log].InsertETLBatchEvent
+            10
+           ,@ETLBatchExecutionId
+           ,NULL
+           ,@EventDescription;
 
           WAITFOR DELAY @PollingDelay;
       END
 
-	  SET @EndETLBatchExecutionInd = 0;
+    SET @EndETLBatchExecutionInd = 0;
 
     RETURN 0 
