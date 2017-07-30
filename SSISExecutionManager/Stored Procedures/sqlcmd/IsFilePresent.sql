503cadda2614828ca2804ca62a6266ac60c96802
David Elish <david.elish@informationcollaboration.com>
2017-07-29 17:50:35 -0700

    Bug fix for proc.

--- SSISExecutionManager/Stored Procedures/sqlcmd/IsFilePresent.sql
+++ SSISExecutionManager/Stored Procedures/sqlcmd/IsFilePresent.sql
@@ -1,12 +1,14 @@
 ﻿CREATE PROCEDURE [sqlcmd].[IsFilePresent] @Path            VARCHAR(512),
                                           @ConditionMetInd BIT OUT
 AS
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
     DECLARE @Result INT
 
     EXEC master.dbo.xp_fileexist
       @Path,
       @Result OUTPUT
+
+    SET @ConditionMetInd = CAST(@Result AS BIT);
 
     RETURN 0 
