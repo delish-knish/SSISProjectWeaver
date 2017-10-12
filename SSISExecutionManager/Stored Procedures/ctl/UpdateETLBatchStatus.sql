fdaff1eb8f0fa06417cf0f7fefe71af6a3548780
David Elish <david.elish@informationcollaboration.com>
2017-10-12 11:55:10 -0400

    Reporting updates. Fix for manual batch cancel being overwritten.

--- SSISExecutionManager/Stored Procedures/ctl/UpdateETLBatchStatus.sql
+++ SSISExecutionManager/Stored Procedures/ctl/UpdateETLBatchStatus.sql
@@ -7,33 +7,36 @@ AS
             ,@TotalETLPackageCount                 SMALLINT = NULL
             ,@TotalRemainingETLPackageCount        SMALLINT = NULL
             ,@ETLBatchExecutionCompleteStatusId    INT = 5
-            ,@ETLBatchExecutionCanceledStatusId    INT = 10;
+            ,@ETLBatchExecutionCanceledStatusId    INT = 10
+			,@ETLBatchExecutionCurrentStatusId	   INT = (SELECT ETLBatchStatusId FROM ctl.ETLBatchExecution WHERE ETLBatchExecutionId = @ETLBatchExecutionId);
 
-;
-    SELECT
-      @TotalEntryPointPackageCount = epb.TotalEntryPointPackageCount
-     ,@TotalRemainingEntryPointPackageCount = epb.TotalRemainingEntryPointPackageCount
-     ,@TotalETLPackageCount = epb.TotalETLPackageCount
-     ,@TotalRemainingETLPackageCount = epb.TotalRemainingETLPackageCount
-     ,@TotalETLPackageCount = epb.TotalETLPackageCount
-     ,@TotalRemainingETLPackageCount = epb.TotalRemainingETLPackageCount
-     ,@ETLBatchExecutionStatusId = epb.ETLBatchExecutionStatusId
-    FROM
-      [dbo].[func_GetETLBatchExecutionStatus] (@ETLBatchExecutionId) epb
+	IF @ETLBatchExecutionCurrentStatusId <> @ETLBatchExecutionCanceledStatusId --The batch was canceled outside of normal processing and we don't want the running process to override/overwrite this
+	BEGIN
+		SELECT
+		  @TotalEntryPointPackageCount = epb.TotalEntryPointPackageCount
+		 ,@TotalRemainingEntryPointPackageCount = epb.TotalRemainingEntryPointPackageCount
+		 ,@TotalETLPackageCount = epb.TotalETLPackageCount
+		 ,@TotalRemainingETLPackageCount = epb.TotalRemainingETLPackageCount
+		 ,@TotalETLPackageCount = epb.TotalETLPackageCount
+		 ,@TotalRemainingETLPackageCount = epb.TotalRemainingETLPackageCount
+		 ,@ETLBatchExecutionStatusId = epb.ETLBatchExecutionStatusId
+		FROM
+		  [dbo].[func_GetETLBatchExecutionStatus] (@ETLBatchExecutionId) epb
 
-    --If the batch has just completed, get the EndDateTime
-    SET @EndDateTime = IIF(@EndDateTime IS NULL
-                           AND @ETLBatchExecutionStatusId IN (@ETLBatchExecutionCompleteStatusId, @ETLBatchExecutionCanceledStatusId), GETDATE(), NULL)
+		--If the batch has just completed, get the EndDateTime
+		SET @EndDateTime = IIF(@EndDateTime IS NULL
+							   AND @ETLBatchExecutionStatusId IN (@ETLBatchExecutionCompleteStatusId, @ETLBatchExecutionCanceledStatusId), GETDATE(), NULL)
 
-    --Update the ETLBatch table
-    EXEC [ctl].[SaveETLBatchExecution]
-      @ETLBatchExecutionId OUT
-     ,@EndDateTime = @EndDateTime
-     ,@TotalEntryPointPackageCount = @TotalEntryPointPackageCount
-     ,@TotalRemainingEntryPointPackageCount = @TotalRemainingEntryPointPackageCount
-     ,@TotalETLPackageCount = @TotalETLPackageCount
-     ,@TotalRemainingETLPackageCount = @TotalRemainingETLPackageCount
-     ,@ETLBatchStatusId = @ETLBatchExecutionStatusId
-     ,@SSISEnvironmentName = NULL
+		--Update the ETLBatch table
+		EXEC [ctl].[SaveETLBatchExecution]
+		  @ETLBatchExecutionId OUT
+		 ,@EndDateTime = @EndDateTime
+		 ,@TotalEntryPointPackageCount = @TotalEntryPointPackageCount
+		 ,@TotalRemainingEntryPointPackageCount = @TotalRemainingEntryPointPackageCount
+		 ,@TotalETLPackageCount = @TotalETLPackageCount
+		 ,@TotalRemainingETLPackageCount = @TotalRemainingETLPackageCount
+		 ,@ETLBatchStatusId = @ETLBatchExecutionStatusId
+		 ,@SSISEnvironmentName = NULL
+	END
 
     RETURN 0 
---
+++ SSISExecutionManager/Views/rpt/ETLPackagesInFailedState.sql
@@ -0,0 +1,61 @@
+﻿CREATE VIEW [rpt].[ETLPackagesInFailedState]
+AS
+  WITH ex
+       AS (SELECT
+             exe.ETLBatchExecutionId
+             ,exe.SSISDBExecutionId
+             ,s.ETLExecutionStatusId
+             ,s.ETLPackageId
+             ,s.StartDateTime
+           FROM
+             [ctl].[ETLBatchSSISDBExecutions] exe
+             CROSS APPLY dbo.func_GetETLPackageExecutionStatusesFromSSISDB(exe.SSISDBExecutionId) s)
+  SELECT
+    fex.ETLBatchExecutionId
+    ,fex.ETLPackageId
+    ,LastPackageFailureStartTime
+    ,LastPackageSuccessStartTime
+    ,fex.FirstErrorMessage
+    ,fex.ErrorDateTime
+  FROM
+    (SELECT
+       ex.ETLBatchExecutionId
+       ,ex.ETLPackageId
+       ,MAX(ex.StartDateTime)  AS LastPackageFailureStartTime
+       ,MAX(err.ErrorMessage)  AS FirstErrorMessage
+       ,MAX(err.ErrorDateTime) AS ErrorDateTime
+     FROM
+       ex
+       OUTER APPLY (SELECT TOP 1
+                      err.ErrorMessage
+                      ,err.ErrorDateTime
+                    FROM
+                      [log].[ETLPackageExecutionError] err
+                    WHERE
+                     ex.[SSISDBExecutionId] = err.[SSISDBExecutionId]
+                    ORDER  BY
+                     ErrorDateTime ASC) err
+     WHERE
+      ex.ETLExecutionStatusId IN ( 3, 4, 6 )
+     GROUP  BY
+      ex.ETLBatchExecutionId
+      ,ex.ETLPackageId
+     HAVING
+      COUNT(*) > 0) fex
+    LEFT JOIN (SELECT
+                 ETLBatchExecutionId
+                 ,ex.ETLPackageId
+                 ,MAX(ex.StartDateTime) AS LastPackageSuccessStartTime
+               FROM
+                 ex
+               WHERE
+                ex.ETLExecutionStatusId = 7
+               GROUP  BY
+                ex.ETLBatchExecutionId
+                ,ex.ETLPackageId
+               HAVING
+                COUNT(*) > 0) sex
+           ON fex.ETLPackageId = sex.ETLPackageId
+		   AND fex.ETLBatchExecutionId = sex.ETLBatchExecutionId
+  WHERE
+    ISNULL(sex.LastPackageSuccessStartTime, '1900-01-01') < fex.LastPackageFailureStartTime 
